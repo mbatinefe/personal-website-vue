f586fd15241ac27a82592c236ff22589e83d9786
mbatinefe <mbefe34@gmail.com>
2024-04-10 18:53:09 +0200

    Add Git-Base

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="resume-view">
       <h1>My Resume</h1>
-      <iframe src="/public/MustafaBatin.pdf" frameborder="0" width="100%" height="500px">
+      <iframe src="/MustafaBatin.pdf" frameborder="0" width="100%" height="500px">
         Your browser does not support PDFs.
-        <a href="/public/MustafaBatin.pdf">Download the PDF</a>.
+        <a href="/MustafaBatin.pdf">Download the PDF</a>.
       </iframe>
       <br/>
-      <a href="/public/MustafaBatin.pdf" download="MustafaBatin.pdf" class="btn btn-primary">Download Resume</a>
+      <a href="/MustafaBatin.pdf" download="MustafaBatin.pdf" class="btn btn-primary">Download Resume</a>
     </div>
   </template>
   
