170f53124f85f1d7f7e91fb15476e9c20c70da1e
mbatinefe <mbefe34@gmail.com>
2024-04-13 17:09:17 +0200

    Change HashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -29,8 +29,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory('/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+  });
 
 export default router;
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="resume-view">
-      <h1>My Resume</h1>
-      <iframe src="/docs/assets/MustafaBatin.pdf" frameborder="0" width="100%" height="500px">
-        Your browser does not support PDFs.
-        <a href="/docs/assets/MustafaBatin.pdf">Download the PDF</a>.
-      </iframe>
-      <br/>
-      <a href="/docs/assets/MustafaBatin.pdf" download="MustafaBatin.pdf" class="btn btn-primary">Download Resume</a>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ResumeView',
-  };
-  </script>
-  
-  <style scoped>
-  .resume-view {
-    text-align: center;
-    padding: 20px;
-  }
-  </style>
-  
-  
+  <div class="resume-view">
+    <h1>My Resume</h1>
+    <iframe src="/docs/assets/MustafaBatin.pdf" frameborder="0" width="100%" height="500px">
+      Your browser does not support PDFs.
+      <a href="/docs/assets/MustafaBatin.pdf">Download the PDF</a>.
+    </iframe>
+    <br/>
+    <a href="/docs/assets/MustafaBatin.pdf" download="MustafaBatin.pdf" class="btn btn-primary">Download Resume</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumeView',
+};
+</script>
+
+<style scoped>
+.resume-view {
+  text-align: center;
+  padding: 20px;
+}
+</style>
+
