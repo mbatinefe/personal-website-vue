226e1f56e9cb382e459a9e855298d5f4c08a306b
mbatinefe <mbefe34@gmail.com>
2024-04-13 17:22:48 +0200

    Change to GitHub Domain

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/',
+  base: '/personal-website-vue/',
   build: {
     outDir: 'docs',
   },
