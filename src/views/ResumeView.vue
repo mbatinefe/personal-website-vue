5b5954c04f698ae596e1755d213e29fbe211f0a6
mbatinefe <mbefe34@gmail.com>
2024-04-13 02:33:40 +0200

    Change CV base

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,32 +1,26 @@
 <template>
-  <div class="resume-view">
-    <h1>My Resume</h1>
-   
-    <iframe :src="pdfUrl" frameborder="0" width="100%" height="500px">
-      Your browser does not support PDFs.
-      <a :href="pdfUrl">Download the PDF</a>.
-    </iframe>
-    <br/>
-    
-    <a :href="pdfUrl" download="MustafaBatin-resume.pdf" class="btn btn-primary">Download Resume</a>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'ResumeView',
-  data() {
-    return {
-      // Include the BASE_URL in the path for proper referencing
-      pdfUrl: `${import.meta.env.BASE_URL}MustafaBatin.pdf`,
-    };
-  },
-};
-</script>
-
-<style scoped>
-.resume-view {
-  text-align: center;
-  padding: 20px;
-}
-</style>
+    <div class="resume-view">
+      <h1>My Resume</h1>
+      <iframe src="./MustafaBatin.pdf" frameborder="0" width="100%" height="500px">
+        Your browser does not support PDFs.
+        <a href="./MustafaBatin.pdf">Download the PDF</a>.
+      </iframe>
+      <br/>
+      <a href="./MustafaBatin.pdf" download="MustafaBatin.pdf" class="btn btn-primary">Download Resume</a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ResumeView',
+  };
+  </script>
+  
+  <style scoped>
+  .resume-view {
+    text-align: center;
+    padding: 20px;
+  }
+  </style>
+  
+  
