041994b28fbeac280df1673825d7348930958f0a
mbatinefe <mbefe34@gmail.com>
2024-04-13 03:28:20 +0200

    Change PDF Source

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/', // Replace <REPO_NAME> with your GitHub repository name.
+  base: '/',
   build: {
     outDir: 'docs',
   },
