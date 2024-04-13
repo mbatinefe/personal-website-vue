8d4006330c96d496d01d3e8e8448291a11bf4f2c
mbatinefe <mbefe34@gmail.com>
2024-04-13 16:33:04 +0200

    Fix 404 Replacement

--- src/router/index.js
+++ src/router/index.js
@@ -29,9 +29,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes,
-  // You can keep duplicateNavigationPolicy if you need it, but make sure it's necessary for your use case.
 });
 
 export default router;
